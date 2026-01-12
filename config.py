3fe9cdaf715093372db135ee6a36a373906e9d54
Chintu9990 <rabinarayannayak182@gmail.com>
2026-01-12 10:13:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+URLS_LIKE = {
+    "IND":"https://clientbp.ggblueshark.com/LikeProfile",
+    "BR":"https://client.ff.blueshark-cloud.com/LikeProfile",
+    "US":"https://client.ff.blueshark-cloud.com/LikeProfile",
+    "SAC":"https://client.ff.blueshark-cloud.com/LikeProfile",
+    "NA":"https://client.ff.blueshark-cloud.com/LikeProfile"
+}
+
+URLS_INFO = {
+    "IND":"https://clientbp.ggblueshark.com/GetPlayerPersonalShow",
+    "BR":"https://client.ff.blueshark-cloud.com/GetPlayerPersonalShow",
+    "US":"https://client.ff.blueshark-cloud.com/GetPlayerPersonalShow",
+    "SAC":"https://client.ff.blueshark-cloud.com/GetPlayerPersonalShow",
+    "NA":"https://client.ff.blueshark-cloud.com/GetPlayerPersonalShow"
+}
+
+FILES = {
+    "IND":"token_ind.json",
+    "BR":"token_br.json",
+    "US":"token_br.json",
+    "SAC":"token_br.json",
+    "NA":"token_br.json"
+}
